0fa06f0b8d84b1af319dbfe07924212fc0567c34
muhammu <52192705+muhammu@users.noreply.github.com>
2021-12-01 22:14:53 +0100

    added low-order mesh for rectangular vertical membrane

--- mesh/__init__.py
+++ mesh/__init__.py
@@ -1,3 +1,19 @@
 # __init__.py
-from .low_order_lib import LO_mesh, hemisphere, hollow_cylinder, rectangular, to_rosetta_format
+from .low_order_lib import LO_mesh, to_rosetta_format
+from .low_order_lib import hemisphere, hollow_cylinder, rectangular, vertical_membrane_cos
 from .gdf_filegen import gdf_LO
+
+# Timing utilities
+import time
+
+class Timer(object):
+    def __init__(self, task):
+        self.task = task
+
+    def __enter__(self):
+        print('STARTING {}'.format(self.task))
+        self.t_start = time.time()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        t = time.time() - self.t_start
+        print('DONE with {} in {:.4f} seconds'.format(self.task, t))
--- mesh/low_order_lib.py
+++ mesh/low_order_lib.py
@@ -8,7 +8,8 @@ class LO_mesh(object):
         """
         self.origo = None
         self.type = None
-        self.symmetry = None
+        self.ISX = None
+        self.ISY = None
         self.wetside = None
         self.panelmat = None
 
@@ -42,6 +43,41 @@ def to_rosetta_format(panel_mat):
         panel_rosetta = np.vstack((panel_rosetta, panel_mat[i, 6:9]))
         panel_rosetta = np.vstack((panel_rosetta, panel_mat[i, 9:12]))
     return panel_rosetta
+
+
+def cosine_spacing(start_point, end_point, num_points):
+    """
+    SHORT DESCRIPTION:
+    generate cosine spaced vector (1xN), where the spacing concentrates samples
+    at the ends while producing fewer sample points in the center.
+
+    INPUT:
+        - start_point = index 1 of the vector
+        - end_point = index N of the vector
+    NOTE THAT END-POINT SHOULD BE GREATER THAN THE START POINT
+
+    OUTPUT:
+    x: 1 x N vector
+
+    Muhammad Mukhlas, Norwegian University of Science and Technology, 2021
+    """
+
+    assert (end_point > start_point), "End point must be greater than the start point"
+    assert (num_points % 2) == 1, "It is recommended to use odd num_points"
+
+    x = np.zeros(num_points)
+    x[0] = start_point
+    x[-1] = end_point
+
+    mid_point = (end_point - start_point) / 2
+    angle_inc = np.pi / (num_points - 1)
+
+    cur_angle = angle_inc
+    for idx in range(1, int(1 + ((num_points -1) / 2))):
+        x[idx] = start_point + (mid_point * (1 - np.cos(cur_angle)))
+        x[-1 - idx] = x[-1 - (idx - 1)] - (x[idx] - x[idx - 1])
+        cur_angle += angle_inc
+    return x
 
 
 def hemisphere(r, n_hoop, n_merid, symm, wet_side, org=None):
@@ -87,9 +123,11 @@ def hemisphere(r, n_hoop, n_merid, symm, wet_side, org=None):
     else:
         mesh.wetside = 'interior'
     if symm == 0:
-        mesh.symmetry = 'none'
+        mesh.ISX = 0
+        mesh.ISY = 0
     else:
-        mesh.symmetry = 'ISX_local + ISY_local'
+        mesh.ISX = 1
+        mesh.ISY = 1
 
     # Define meridional (theta) and hoop (phi) angles, in radians
     theta = np.linspace(np.pi / 2, np.pi, n_merid + 1)
@@ -207,9 +245,11 @@ def hollow_cylinder(r_in, r_out, h, n_hoop, n_r, n_h, symm, wet_side, org=None):
     else:
         mesh.wetside = 'interior'
     if symm == 0:
-        mesh.symmetry = 'none'
+        mesh.ISX = 0
+        mesh.ISY = 0
     else:
-        mesh.symmetry = 'ISX_local + ISY_local'
+        mesh.ISX = 1
+        mesh.ISY = 1
 
     # cylinder base
     r_dir = np.linspace(r_in, r_out, n_r + 1)
@@ -353,9 +393,11 @@ def rectangular(B, H, n_B, n_H, symm, wet_side, org=None):
     else:
         mesh.wetside = 'back'
     if symm == 0:
-        mesh.symmetry = 'none'
+        mesh.ISX = 0
+        mesh.ISY = 0
     else:
-        mesh.symmetry = 'ISY_local'
+        mesh.ISX = 0
+        mesh.ISY = 1
 
     y_rect, z_rect = np.meshgrid(B_dir, H_dir)
     x_rect = np.zeros_like(y_rect)
@@ -405,6 +447,196 @@ def rectangular(B, H, n_B, n_H, symm, wet_side, org=None):
             v_4_y = np.append(v_4_y, y_rect[j, 1:n_B + 1])
             v_4_z = np.append(v_4_z, z_rect[j, 1:n_B + 1])
         #
+    # Array (total panel x 12 coord for vertex 1-4) which it should be okay to use for WAMIT
+    mesh.panelmat = np.transpose(np.array([v_1_x, v_1_y, v_1_z,
+                                           v_2_x, v_2_y, v_2_z,
+                                           v_3_x, v_3_y, v_3_z,
+                                           v_4_x, v_4_y, v_4_z]))
+    return mesh
+
+
+def vertical_membrane_cos(B, H, n_B, th, symm, wet_side, m=1, org=None):
+    """
+    SHORT DESCRIPTION:
+    Generate low-order mesh of a vertical surface penetrating membrane
+    with finite thickness th. The mesh spacing along the breadth uses
+    the so-called "cosine spacing", i.e. area near the membrane edges
+    is more refined.
+
+    INPUT:
+    - B: breadth of the membrane [m]
+    - H: total submerged height of the membrane [m]
+    - m: multiplied of dz (spacing along height), where in default it is equal to 1
+    - th: membrane thickness
+    - n_B: number of panel along the breadth
+    - symm: symmetry of the body.
+            0 = no symmetry (HAVE NOT BEEN INCLUDED YET)
+            1 = symmetry (1/4th of the body)
+    - wet_side:
+            0 = wet outside of the membrane
+            1 = wet inside of the membrane
+
+    OUTPUT:
+    mesh = i x j matrix
+    where:
+     i = panel number,
+     j = vertex number for each panel (always 12)
+
+    Muhammad Mukhlas, Norwegian University of Science and Technology, 2021
+    """
+    if org is None:   # if not declared, set [0., 0., 0.] as default
+        org = np.array([0., 0., 0.])
+    assert wet_side == 0 or wet_side == 1, "please set wet_side = 0 or 1"
+    assert symm == 1, "please set symm = 1" # TODO: under development
+
+    # number of points in breadth direction
+    if (n_B % 2) != 0:
+        n_B += 1
+    num_points = n_B + 1
+
+    # Generate cosine spacing vector for the meshes in breadth direction
+    B_vec = (B * cosine_spacing(0, 1, num_points)) - (B / 2)
+
+    if symm == 1:
+        B_dir = B_vec[int((num_points - 1) / 2):] + org[1]
+        n_B = len(B_dir) - 1
+        dz = 0.5 * m * th
+        n_H = int(np.ceil(H / dz))
+        H_dir = - np.linspace(0, H, n_H + 1, endpoint=True) + org[2]
+    elif symm == 0:  # TODO: under development
+        B_dir = B_vec + org[1]
+        n_B = len(B_dir) - 1
+        dz = th * m
+        n_H = int(np.ceil(H / dz))
+        H_dir = - np.linspace(0, H, n_H + 1, endpoint=True) + org[2]
+
+    # Set the body mesh as an instance
+    mesh = LO_mesh()
+    mesh.origo = org
+    mesh.type = 'vertical membrane th'
+    if wet_side == 0:
+        mesh.wetside = 'exterior'
+    else:
+        mesh.wetside = 'interior'
+    if symm == 0:  # TODO: under development
+        mesh.ISX = 0
+        mesh.ISY = 0
+    else:
+        mesh.ISX = 1
+        mesh.ISY = 1
+
+    # face
+    y_rect, z_rect = np.meshgrid(B_dir, H_dir)
+    x_rect = np.zeros_like(y_rect) + (th / 2)
+
+    # bottom
+    x_bot, y_bot = np.meshgrid(np.array([0, th / 2]), B_dir)
+    z_bot = np.zeros_like(x_bot) - H
+
+    # side
+    x_side, z_side = np.meshgrid(np.array([0, th / 2]), H_dir)
+    y_side = np.zeros_like(x_side) + (B / 2)
+
+    # Make blank array for each vertex
+    v_1_x = np.zeros(0)
+    v_1_y = np.zeros(0)
+    v_1_z = np.zeros(0)
+    #
+    v_2_x = np.zeros(0)
+    v_2_y = np.zeros(0)
+    v_2_z = np.zeros(0)
+    #
+    v_3_x = np.zeros(0)
+    v_3_y = np.zeros(0)
+    v_3_z = np.zeros(0)
+    #
+    v_4_x = np.zeros(0)
+    v_4_y = np.zeros(0)
+    v_4_z = np.zeros(0)
+    #
+    # Membrane face
+    for j in range(0, n_H):
+        v_1_x = np.append(v_1_x, x_rect[j, :n_B])
+        v_1_y = np.append(v_1_y, y_rect[j, :n_B])
+        v_1_z = np.append(v_1_z, z_rect[j, :n_B])
+        #
+        v_3_x = np.append(v_3_x, x_rect[j + 1, 1:n_B + 1])
+        v_3_y = np.append(v_3_y, y_rect[j + 1, 1:n_B + 1])
+        v_3_z = np.append(v_3_z, z_rect[j + 1, 1:n_B + 1])
+        #
+        # WAMIT has convention to determine which side is wet and vice versa
+        if wet_side == 1:
+            v_2_x = np.append(v_2_x, x_rect[j, 1:n_B + 1])
+            v_2_y = np.append(v_2_y, y_rect[j, 1:n_B + 1])
+            v_2_z = np.append(v_2_z, z_rect[j, 1:n_B + 1])
+            #
+            v_4_x = np.append(v_4_x, x_rect[j + 1, :n_B])
+            v_4_y = np.append(v_4_y, y_rect[j + 1, :n_B])
+            v_4_z = np.append(v_4_z, z_rect[j + 1, :n_B])
+        elif wet_side == 0:
+            v_2_x = np.append(v_2_x, x_rect[j + 1, :n_B])
+            v_2_y = np.append(v_2_y, y_rect[j + 1, :n_B])
+            v_2_z = np.append(v_2_z, z_rect[j + 1, :n_B])
+            #
+            v_4_x = np.append(v_4_x, x_rect[j, 1:n_B + 1])
+            v_4_y = np.append(v_4_y, y_rect[j, 1:n_B + 1])
+            v_4_z = np.append(v_4_z, z_rect[j, 1:n_B + 1])
+        #
+    # Membrane side
+    for j in range(0, n_H):
+        v_1_x = np.append(v_1_x, x_side[j, 0])
+        v_1_y = np.append(v_1_y, y_side[j, 0])
+        v_1_z = np.append(v_1_z, z_side[j, 0])
+        #
+        v_3_x = np.append(v_3_x, x_side[j + 1, 1])
+        v_3_y = np.append(v_3_y, y_side[j + 1, 1])
+        v_3_z = np.append(v_3_z, z_side[j + 1, 1])
+        #
+        # WAMIT has convention to determine which side is wet and vice versa
+        if wet_side == 0:
+            v_2_x = np.append(v_2_x, x_side[j, 1])
+            v_2_y = np.append(v_2_y, y_side[j, 1])
+            v_2_z = np.append(v_2_z, z_side[j, 1])
+            #
+            v_4_x = np.append(v_4_x, x_side[j + 1, 0])
+            v_4_y = np.append(v_4_y, y_side[j + 1, 0])
+            v_4_z = np.append(v_4_z, z_side[j + 1, 0])
+        elif wet_side == 1:
+            v_2_x = np.append(v_2_x, x_side[j + 1, 0])
+            v_2_y = np.append(v_2_y, y_side[j + 1, 0])
+            v_2_z = np.append(v_2_z, z_side[j + 1, 0])
+            #
+            v_4_x = np.append(v_4_x, x_side[j, 1])
+            v_4_y = np.append(v_4_y, y_side[j, 1])
+            v_4_z = np.append(v_4_z, z_side[j, 1])
+        #
+    # Membrane bottom
+    for j in range(0, n_B):
+        v_1_x = np.append(v_1_x, x_bot[j, 0])
+        v_1_y = np.append(v_1_y, y_bot[j, 0])
+        v_1_z = np.append(v_1_z, z_bot[j, 0])
+        #
+        v_3_x = np.append(v_3_x, x_bot[j + 1, 1])
+        v_3_y = np.append(v_3_y, y_bot[j + 1, 1])
+        v_3_z = np.append(v_3_z, z_bot[j + 1, 1])
+        #
+        # WAMIT has convention to determine which side is wet and vice versa
+        if wet_side == 0:
+            v_2_x = np.append(v_2_x, x_bot[j + 1, 0])
+            v_2_y = np.append(v_2_y, y_bot[j + 1, 0])
+            v_2_z = np.append(v_2_z, z_bot[j + 1, 0])
+            #
+            v_4_x = np.append(v_4_x, x_bot[j, 1])
+            v_4_y = np.append(v_4_y, y_bot[j, 1])
+            v_4_z = np.append(v_4_z, z_bot[j, 1])
+        elif wet_side == 1:
+            v_2_x = np.append(v_2_x, x_bot[j, 1])
+            v_2_y = np.append(v_2_y, y_bot[j, 1])
+            v_2_z = np.append(v_2_z, z_bot[j, 1])
+            #
+            v_4_x = np.append(v_4_x, x_bot[j + 1, 0])
+            v_4_y = np.append(v_4_y, y_bot[j + 1, 0])
+            v_4_z = np.append(v_4_z, z_bot[j + 1, 0])
     # Array (total panel x 12 coord for vertex 1-4) which it should be okay to use for WAMIT
     mesh.panelmat = np.transpose(np.array([v_1_x, v_1_y, v_1_z,
                                            v_2_x, v_2_y, v_2_z,
